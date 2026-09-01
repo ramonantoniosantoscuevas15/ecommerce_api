7c0cc0e70e6cce1eed53945fc804eab686392af8
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2026-09-01 16:14:21 -0400

    preparando el almacenamiento de fotos

--- ecommerce_api/Entidades/Product.cs
+++ ecommerce_api/Entidades/Product.cs
@@ -1,10 +1,13 @@
-﻿namespace ecommerce_api.Entidades
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce_api.Entidades
 {
     public class Product : BaseEntity
     {
         public required string Nombre { get; set; }
         public required string Descripcion { get; set; }
         public decimal Precio { get; set; }
+        [Unicode(false)]
         public required string FotoUrl { get; set; }
         public required string Marca { get; set; }
     }
