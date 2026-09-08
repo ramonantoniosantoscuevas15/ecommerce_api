5acb40b837dd775a460c8e8639e1a995aed0b680
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2026-09-08 11:27:31 -0400

    creando las categorias

---
+++ ecommerce_api/Entidades/Categorie.cs
@@ -0,0 +1,6 @@
+﻿namespace ecommerce_api.Entidades
+{
+    public class Categorie
+    {
+    }
+}
--- ecommerce_api/Entidades/Category.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ecommerce_api.Entidades
-{
-    public class Category: BaseEntity
-    {
-        public required string Tipo { get; set; }
-        public string? Talla { get; set; }
-    }
-}
