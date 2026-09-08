1e721285f6114742ec2ca2daf4c0b9e364073878
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2026-09-08 16:01:19 -0400

    creando entidades

---
+++ ecommerce_api/Entidades/Imagen.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce_api.Entidades
+{
+    public class Imagen:BaseEntity
+    {
+        [Unicode(false)]
+        public required string Url { get; set; }
+    }
+}
