239a70e1242ee7d7910e37d1c9bcc8e8b956b046
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2026-09-09 11:10:08 -0400

    implementando la interfacas

---
+++ ecommerce_api/Interfaces/Icategorie.cs
@@ -0,0 +1,16 @@
+﻿using ecommerce_api.Entidades;
+namespace ecommerce_api.Interfaces
+
+{
+    public interface ICategorie
+    {
+        Task<IReadOnlyList<Categorie>> GetCategoriesAsync(string?name);
+        Task<Categorie?>GetCategorieByIdAsync(int id);
+        Task<IReadOnlyList<string>> GetNameAsync();
+        void AddCategorie(Categorie categorie);
+        void UpdateCategorie(Categorie categorie);
+        void DeleteCategorie(Categorie categorie);
+        bool CategorieExists(int id);
+        Task<bool> SaveChangesAsync();
+    }
+}
