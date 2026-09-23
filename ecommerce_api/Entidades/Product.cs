aed9b86c58319f75cc372f5185e203e8f20816c4
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2026-09-23 09:15:13 -0400

    agregando los productos

--- ecommerce_api/Entidades/Product.cs
+++ ecommerce_api/Entidades/Product.cs
@@ -4,11 +4,13 @@ namespace ecommerce_api.Entidades
 {
     public class Product : BaseEntity
     {
-        public required string Nombre { get; set; }
-        public required string Descripcion { get; set; }
-        public decimal Precio { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public decimal Price { get; set; }
         [Unicode(false)]
-        public required string FotoUrl { get; set; } 
-        public required string Marca { get; set; }
+        public required string PictureUrl { get; set; } 
+        public required string Brand { get; set; }
+        public required string Type { get; set; }
+        public int QuantityInStock { get; set; }
     }
 }
---
+++ ecommerce_api/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ecommerce_api.Entidades;
+
+namespace ecommerce_api.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<Product?> GetProductByIdAsync(int id);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(Product product);
+        bool ProductExists(int id);
+        Task<bool> SaveChangesAsync();
+    }
+}
